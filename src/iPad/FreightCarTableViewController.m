80d95d31a35b948973407a94d434232b2f794fbe
rwbowdidge@gmail.com <rwbowdidge@gmail.com@eab13115-e324-4862-c895-1835f8a8b02b>
2015-01-31 09:49:15 +0000

    Initialize each new freight car with a non-duplicate number. Useful for creating a bunch during testing.

--- src/iPad/FreightCarTableViewController.m
+++ src/iPad/FreightCarTableViewController.m
@@ -66,7 +66,15 @@
     AppDelegate *myAppDelegate = (AppDelegate*) [UIApplication sharedApplication].delegate;
     EntireLayout *entireLayout = myAppDelegate.entireLayout;
  
-    FreightCar *fc = [entireLayout createFreightCar: @"AA&A 84712" withCarType: @"XM" withLength: [NSNumber numberWithInt: 40]];
+    // Create each freight car with a new number.
+    int number = 84709;
+    NSString *reportingMarks;
+    do {
+        number++;
+        reportingMarks = [NSString stringWithFormat: @"AA&A %d", number];
+    } while ([entireLayout freightCarWithName: reportingMarks] != nil);
+    
+    FreightCar *fc = [entireLayout createFreightCar: reportingMarks withCarType: @"XM" withLength: [NSNumber numberWithInt: 40]];
     [fc setCurrentLocation: [entireLayout workbenchIndustry]];
     
     [self regenerateTableData];
